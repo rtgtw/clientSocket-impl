24ee83813816650dc6d9db6a0a32fb4885183af2
Remi Olateru <remiolateru@gmail.com>
2024-04-29 18:24:44 -0400

    Send files to server

--- clientSocket.cpp
+++ clientSocket.cpp
@@ -4,6 +4,8 @@
 #include <WinSock2.h>
 #include <ws2tcpip.h>
 #include <iostream>	
+#include <string>
+#include <fstream>
 
 int main() {
 
@@ -12,6 +14,9 @@ int main() {
 
 	//Specify which port to connect to in an int variable
 	int port = 55555;
+
+	//----------------------------------------------------------------------//
+	//----------------------------------------------------------------------//
 
 	//Initialize the DLL
 
@@ -34,6 +39,10 @@ int main() {
 	}
 
 
+
+	//----------------------------------------------------------------------//
+	//----------------------------------------------------------------------//
+
 	//Create a socket
 	//This specifies the protocol to use, either TCP or UDP
 	 //TCP socket
@@ -54,6 +63,11 @@ int main() {
 
 
 
+	//----------------------------------------------------------------------//
+	//----------------------------------------------------------------------//
+
+
+
 	//After creating the socket, now we have to connect to server
 	//The binding happens automatically by the OS which assigns a port when we call the connect function
 
@@ -71,79 +85,108 @@ int main() {
 	}else{
 		std::cout << "Client: Connected! " << std::endl;
 		std::cout << "Client: Can start sending and receiving data..." << std::endl;
-	
-	
+
 	}
 
 
-
+	//----------------------------------------------------------------------//
+	//----------------------------------------------------------------------//
+	
 	//A new socket is not created for the client, the socket used to connect to the server
 	//is the same socket used for the client
 
+	//File transfer client side
+	//First we have to create a string for the file path
+	std::string filePath;
+	std::cout << "Hey , welcome to the Server :)" << std::endl;
+	std::cout << "Enter file path: ";
+	//get the line for the file
+	std::getline(std::cin, filePath);
+
+	std::cout << "Got file path which is, " << filePath << std::endl;
+
+	system("pause");
 
 
-	//TCP requires a connection to be established
-	//We can use the send() function to send data
-	//Send will return an int of the number of bytes sent, or socket error if unsuccesful
-	//We need to create a buffer that will be used to send the data 
-
-
-	for (int i = 0; i < 1000; i++) {
-		//SEND
-
-		const int bufferSize = 200;
-
-		//create buffer
-		char buffer[bufferSize];
-
-
-
-		//Enter message to be sent to server/client
-		std::cout << "Enter your message:";
-
-		//Store message in buffer
-		std::cin.getline(buffer, bufferSize);
-
-		//use the send function to send out the message, and do error handling
-		//send returns an int of the amount of bytes sent
-		int byteCount = send(clientSocket, buffer, bufferSize, 0);
-		if (byteCount == SOCKET_ERROR) {
-			std::cout << "Sever send error: " << WSAGetLastError() << std::endl;
-			return -1;
-		}
-		else {
-			/*std::cout << "Success! Server received: " << byteCount << " bytes of data.." << std::endl;*/
-		}
+	//open the file to be read
+	std::ifstream inputFile(filePath, std::ios::binary);
+	if (!inputFile) {
+		std::cerr << "Failed to open file for reading" << std::endl;
+		//close socket and cleanup dll
+		system("pause");
 
 
 
+		closesocket(clientSocket);
+		WSACleanup();
+		return -1;
 
-
-
-		//RECIEVE
-		//Recieve data incoming from the client with recv
-		//almost the same process
-		const int recievebufferSize = 200;
-		char recieveBuffer[recievebufferSize];
-
-
-
-		int recievebyteCount = recv(clientSocket, recieveBuffer, recievebufferSize, 0);
-		if (recievebyteCount < 0) {
-			std::cout << "Server: error " << WSAGetLastError;
-			return 0;
-		}
-		else {
-			//If its successful the recieve buffer will be the reply
-			std::cout << "Server: " << recieveBuffer << std::endl;
-
-		}
 
 
 	}
+	else {
+		std::cout << "Success, reading file in input file" << std::endl;
+	}
 
-	std::cin.get();
+	
+	inputFile.seekg(0, std::ios::end);
+	int fileSize = inputFile.tellg();
+	inputFile.seekg(0, std::ios::beg);
+
+	//Send data in chunks, create buffer
+	const int bufferSize = 1024;
+	char buffer[bufferSize];
+
+	//Create a bytes read since send returns the number of bytes
+	int bytesRead;
+	std::cout << "right before while loop" << std::endl;
+	
+
+	inputFile.read(buffer, fileSize);
+
+	/*std::cout << fileSize << std::endl;
+	*/
+
+	
+	send(clientSocket, buffer, fileSize, 0);
+
+	std::cout << "Sent data?" << std::endl;
+
+
+	//while ((bytesRead = inputFile.read(buffer, bufferSize)) > 0) {
+	//	if (send(clientSocket, buffer, bytesRead, 0) == 0) {
+	//		std::cerr << "Send failed: " << WSAGetLastError() << std::endl;
+	//		//close the inputfile
+	//		inputFile.close();
+	//		//close the socket
+	//		closesocket(clientSocket);
+	//		//clean up dll
+	//		WSACleanup();
+	//		return -1;
+	//	}
+	//	else {
+	//		std::cout << "Send went thru, here is the bytes size: " << bytesRead << std::endl;
+	//	}
+	//}
+
+
+
+
+
+	//close the file after its done reading it to the buffer
+	std::cout << "closed" << std::endl;
 	system("pause");
+	inputFile.close();
+
+
+
+	
+
+
+
+	
+	
+	closesocket(clientSocket);
 	WSACleanup();
 
 
@@ -153,3 +196,79 @@ int main() {
 	return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for (int i = 0; i < 1000; i++) {*/
+		//SEND
+
+		//const int bufferSize = 200;
+
+		////create buffer
+		//char buffer[bufferSize];
+
+
+
+		////Enter message to be sent to server/client
+		//std::cout << "Enter your message:";
+
+		////Store message in buffer
+		//std::cin.getline(buffer, bufferSize);
+
+		////use the send function to send out the message, and do error handling
+		////send returns an int of the amount of bytes sent
+		//int byteCount = send(clientSocket, buffer, bufferSize, 0);
+		//if (byteCount == SOCKET_ERROR) {
+		//	std::cout << "Sever send error: " << WSAGetLastError() << std::endl;
+		//	return -1;
+		//}
+		//else {
+		//	/*std::cout << "Success! Server received: " << byteCount << " bytes of data.." << std::endl;*/
+		//}
+
+
+
+
+
+
+		//RECIEVE
+		//Recieve data incoming from the client with recv
+		//almost the same process
+		//const int recievebufferSize = 200;
+		//char recieveBuffer[recievebufferSize];
+
+
+
+		//int recievebyteCount = recv(clientSocket, recieveBuffer, recievebufferSize, 0);
+		//if (recievebyteCount < 0) {
+		//	std::cout << "Server: error " << WSAGetLastError;
+		//	return 0;
+		//}
+		//else {
+		//	//If its successful the recieve buffer will be the reply
+		//	std::cout << "Server: " << recieveBuffer << std::endl;
+
+		//}
+
+
+	/*}*/
